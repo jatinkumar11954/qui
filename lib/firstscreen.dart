a0ebaf9f51f862a9607e96dd5003d39bc80ba68e
jatinkumar11954 <jatinkumar11954@gmail.com>
2020-05-09 00:02:34 +0530

    terinary

--- lib/firstscreen.dart
+++ lib/firstscreen.dart
@@ -10,32 +10,37 @@ class FirstScreen extends StatelessWidget {
         print(i);
     return Scaffold(
       body: Container(
+        height: MediaQuery.of(context).size.height*0.8,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            FutureBuilder(
-            future: FirebaseAuth.instance.currentUser(),
-            // . then((user){
-            //       if(user!=null){
-            //          print("Navigator.of(context).pop();");
-            //          Text('Loading...');
-            //           print("Navigator.of(context).pop();");
-            //       }
-            //       else{
-            //         print("Navigator");
-            //       }
-            //     }),
-            builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text("\n   Gmail ID:  "+snapshot.data.phoneNumber,style: TextStyle(color:Colors.red,fontSize:SizeConfig.blockSizeVertical * 2.5),),
-                );
-              }
-              else {
-                return Text('Loading...');
-              }
-            },
+            Container(
+              height: MediaQuery.of(context).size.height*0.5,
+              child: FutureBuilder(
+              future: FirebaseAuth.instance.currentUser(),
+              // . then((user){
+              //       if(user!=null){
+              //          print("Navigator.of(context).pop();");
+              //          Text('Loading...');
+              //           print("Navigator.of(context).pop();");
+              //       }
+              //       else{
+              //         print("Navigator");
+              //       }
+              //     }),
+              builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.all(28.0),
+                    child: Text(i=="gmail"?"\n   Gmail ID:  "+snapshot.data.email:" Phone No:  "+snapshot.data.phoneNumber,style: TextStyle(color:Colors.red,fontSize:SizeConfig.blockSizeVertical * 2.5),),
+                  );
+                }
+                else {
+                  return Text('Loading...');
+                }
+              },
     ),
+            ),
     Padding(
       padding: const EdgeInsets.all(58.0),
       child: FlatButton(onPressed: (){signOutGoogle(); FirebaseAuth.instance
@@ -43,7 +48,7 @@ class FirstScreen extends StatelessWidget {
                   .then((result) =>
                       Navigator.pushReplacementNamed(context, "login"))
                   .catchError((err) => print(err));
-      }, child: Text("Logout")),
+      }, child: Text("Logout",style: TextStyle(color:Colors.green,fontSize:35),)),
     )
           ],
         ),
